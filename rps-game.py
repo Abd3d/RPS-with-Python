86377817d16145321581588845a795ec466d9948
Abd3d <fanshawe3d@gmail.com>
2022-03-20 20:09:25 -0400

    Step2: set total of rounds between one to ten with validation

--- rps-game.py
+++ rps-game.py
@@ -14,8 +14,7 @@ playerChooice = ""
 computerChooice = ""
 numberOfRounds = 0
 playerChooice = ""
-playerLives = numberOfRounds
-computerLives = numberOfRounds
+
 # =========== Step 1: Game Rules ===========
 print(Fore.CYAN + '***********************************')
 print("\tWelcom " + playerName.upper() +
@@ -26,3 +25,26 @@ print("\t" f'{Fore.GREEN}Rock     {Fore.YELLOW}Beats {Fore.RED}Scissors')
 print("\t"f'{Fore.GREEN}Scissors {Fore.YELLOW}Beats {Fore.RED}Paper')
 print("\t"f'{Fore.GREEN}Paper    {Fore.YELLOW}Beats {Fore.RED}Rock')
 print(Fore.CYAN + '***********************************')
+
+# ============= Validation Input Function :
+# Step2: inputRounds: to check if the input is number and between 1 to 10
+
+
+def validation(value):
+    global numberOfRounds
+    global playerChooice
+    if(value == 'inputRounds'):
+        while numberOfRounds == 0:
+            numberOfRounds = input(
+                "How many rounds do you want to play? between [1 to 10]: ")
+            if (numberOfRounds.isnumeric() is True):
+                numberOfRounds = int(numberOfRounds)
+                if (numberOfRounds < 1 or numberOfRounds > 10):
+                    numberOfRounds = 0
+            else:
+                numberOfRounds = 0
+
+
+validation('inputRounds')
+playerLives = numberOfRounds
+computerLives = numberOfRounds
